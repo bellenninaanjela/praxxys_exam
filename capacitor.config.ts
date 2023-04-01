1c534942f4acec06ffadf606894988fdece6129d
Nina Anjela Bellen~ <ninaanjelaalarcon.bellen>
2023-04-01 20:11:52 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'praxxys_exam',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
